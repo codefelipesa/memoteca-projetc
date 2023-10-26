e75a4c70be8b7693bb71cc5900abda9629e32e02
Felipe <felipesa1411@.com>
2023-10-26 15:30:48 -0300

    ,

---
+++ src/app/componentes/pensamentos/delete-pensamentos/delete-pensamentos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-pensamentos',
+  templateUrl: './delete-pensamentos.component.html',
+  styleUrls: ['./delete-pensamentos.component.css']
+})
+export class DeletePensamentosComponent {
+  excluirPensamento(){
+    
+  }
+}
